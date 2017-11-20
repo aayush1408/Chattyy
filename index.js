cbb22cae1afabbd9fe1928abf63fdeadc98c5578
Aayush <aayushjaiswal0114@gmail.com>
2017-11-21 01:55:58 +0530

    Completed app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 let app = express();
 
 //api running
@@ -9,4 +10,18 @@ let server = app.listen(5000,()=>{
 //public files
 app.use(express.static('public'));
 
+//socket
+let io = socket(server);
+
+io.on('connection',(socket)=>{
+    console.log('Connection made '+socket.id);
+
+    socket.on('chat',(data)=>{
+        console.log(data);
+        io.sockets.emit('chat',data);
+    });
+     socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+});
 
